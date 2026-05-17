aba4d7613811995fac7d8219c6ccb3af6d34b544
c0lbarator <57714998+c0lbarator@users.noreply.github.com>
2026-05-17 14:51:13 +0300

    trying to fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,6 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  base: '/presentation/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
